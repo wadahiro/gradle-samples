3e78753e367deae1aa3c2ced069790075a5e61f7
Hiroyuki Wada <wadahiro@gmail.com>
2012-03-20 18:20:28 +0900

    add java-war-sample

---
+++ java-war-sample/common/src/main/java/org/example/servlet/SampleServlet.java
@@ -0,0 +1,7 @@
+package org.example.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class SampleServlet extends HttpServlet {
+
+}
---
+++ java-war-sample/src/main/java/org/example/servlet/SampleServlet2.java
@@ -0,0 +1,7 @@
+package org.example.servlet;
+
+import javax.servlet.http.HttpServlet;
+
+public class SampleServlet2 extends HttpServlet {
+
+}
